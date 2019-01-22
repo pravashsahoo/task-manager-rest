d9c8fb7244eeb5c280f61b184848675201022e9f
pravash <pravashsahoo@gmail.com>
2019-01-22 19:56:35 +0530

    first commit for rest

---
+++ src/main/java/com/cts/task/taskmanagerrest/TaskManagerRestApplication.java
@@ -0,0 +1,14 @@
+package com.cts.task.taskmanagerrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerRestApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/cts/task/taskmanagerrest/TaskManagerRestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cts.task.taskmanagerrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskManagerRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
